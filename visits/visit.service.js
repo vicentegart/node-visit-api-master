63e180674daec404fe5ac1a3d7de032f7541c6e2
Vicente GP <vicente.gart@gmail.com>
2022-06-29 10:00:30 +0200

    api code for PT

---
+++ visits/visit.service.js
@@ -0,0 +1,23 @@
+﻿// users hardcoded for simplicity, store in a db for production applications
+const visits = []
+
+module.exports = {
+	visit,
+	getAll,
+}
+
+async function visit({ address, ip }) {
+	if (address && ip) {
+		const id = generateRandomInteger(1000)
+		visits.push({ id, address, ip })
+		return { id, address, ip }
+	}
+}
+
+async function getAll() {
+	return visits
+}
+
+function generateRandomInteger(max) {
+	return Math.floor(Math.random() * max) + 1
+}
---
+++ visits/visits.controller.js
@@ -0,0 +1,27 @@
+﻿const express = require("express")
+const router = express.Router()
+const visitService = require("./visit.service")
+
+// routes
+router.post("/visit", visit)
+router.get("/", getAll)
+
+module.exports = router
+
+function visit(req, res, next) {
+	visitService
+		.visit(req.body)
+		.then((visit) =>
+			visit
+				? res.json(visit)
+				: res.status(400).json({ message: "Invalid input" })
+		)
+		.catch((err) => next(err))
+}
+
+function getAll(req, res, next) {
+	visitService
+		.getAll()
+		.then((visits) => res.json(visits))
+		.catch((err) => next(err))
+}
